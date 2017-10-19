910a2dd23cc3c9a144ef782e6b4bb0b541b13638
Madison Malone <mmmalone@wu-163727.willamette.edu>
2017-10-19 09:37:16 -0700

    Initial Commit

---
+++ HabitTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HabitTracker
+//
+//  Created by Madison M Malone on 10/19/17.
+//  Copyright © 2017 Madison M Malone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
